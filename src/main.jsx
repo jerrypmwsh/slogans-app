9f027a97e493fd7ac1add78bb31f2e18d76c1469
jerrypmwsh <jerrypmwsh@gmail.com>
2023-02-04 22:39:39 -0800

    fix: use href instead

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       clientId="KvRHjbjqF7kjvpm3jnXixUcazvGbVO7k"
       authorizationParams={{
         audience: "https://tresosos.com/slogans",
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.href,
       }}
     >
       <App />
